5dbe4ba5491d2a9f25bb9ec8dec2de2d5be63013
Ian Waddington <iwaddo@icloud.com>
2022-11-21 22:27:20 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ian Waddington on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
