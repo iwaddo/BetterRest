4e83cd934ee6d10c11bf76cbe5fe50d9902a1986
Ian Waddington <iwaddo@icloud.com>
2022-11-23 16:55:40 +0000

    Day 27 - Project 4, part 2 - Before Building a basic layout

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text(Date.now, format: .dateTime.day().month().year())
+            
+            Text(Date.now.formatted(date: .long, time: .shortened))
+            
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            
+            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+            
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+                .labelsHidden()
+            
         }
-        .padding()
     }
+    func moreTrivia() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+    }
+    
+    func trivialExample() {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? Date.now
+        
+    }
+    
+    func exampleDate() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
